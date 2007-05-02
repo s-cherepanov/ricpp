52865058a2a644ae7af2f25a06d78e4523f641d1
pidde <pidde@b61f6476-052f-0410-ab0a-e49323056755>
2007-05-02 11:53:44 +0000

    Difficulties with eol

--- ricpp/source/rendercontext/stdafx.h
+++
@@ -1,5 +0,0 @@
-/*! \file Include file for standard system include files,
- *        or project specific include files that are used frequently, but
- *        are changed infrequently
- */
-#include "tools/ricppplatform.h"

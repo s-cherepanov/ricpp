719a6e5b0b8aaaf74bcdb3ffe740a9c8d624143e
pidde <pidde@b61f6476-052f-0410-ab0a-e49323056755>
2007-05-20 13:33:32 +0000

    inlinetools.h (typo)

--- ricpp2/source/include/ricpp/tools/inlinetools.h
+++ ricpp2/source/include/ricpp/tools/inlinetools.h
@@ -25,7 +25,7 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-/** @file inlinetoosl.h
+/** @file inlinetools.h
  *  @author Andreas Pidde (andreas@pidde.de)
  *  @brief Frequent used inline tools
  */

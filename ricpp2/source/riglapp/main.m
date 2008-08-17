b7f1fd8a381e74a7c8df719b022d18c45871b027
pidde <pidde@b61f6476-052f-0410-ab0a-e49323056755>
2008-08-17 12:28:53 +0000

    native eoln

--- ricpp2/source/riglapp/main.m
+++ ricpp2/source/riglapp/main.m
@@ -1,1 +1,20 @@
-////  main.m//#import <Cocoa/Cocoa.h>#import "ricpp/ri/ri.h"int main(int argc, char *argv[]){	const char *searchPath = ".:&";    int ret = 0;	RiCPPControl("searchpath", "renderer", &searchPath, RI_NULL);	RiBegin("glrenderer");	RiWorldBegin();	// No GL Context here	RiWorldEnd();	ret = NSApplicationMain(argc,  (const char **) argv);	RiEnd();	return ret;}
+//
+//  main.m
+//
+
+#import <Cocoa/Cocoa.h>
+#import "ricpp/ri/ri.h"
+
+int main(int argc, char *argv[])
+{
+	const char *searchPath = ".:&";
+    int ret = 0;
+	RiCPPControl("searchpath", "renderer", &searchPath, RI_NULL);
+	RiBegin("glrenderer");
+	RiWorldBegin();
+	// No GL Context here
+	RiWorldEnd();
+	ret = NSApplicationMain(argc,  (const char **) argv);
+	RiEnd();
+	return ret;
+}

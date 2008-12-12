1d434aab6a1f5e0bb749f93c9ce0a63d35ae66f0
pidde <pidde@b61f6476-052f-0410-ab0a-e49323056755>
2008-12-12 15:21:37 +0000

    Added master headers

---
+++ trunk/ricpp2/source/include/RiCPPBridge.h
@@ -0,0 +1,8 @@
+#ifndef __RICPPTRIANGLE__
+#define __RICPPTRIANGLE__
+
+#ifndef _RICPP_RICPPBRIDGE_TRIANGLERENDERER_H
+#include <ricpp/ricppbridge/trianglerenderer.h>
+#endif
+
+#endif   /* __RICPPTRIANGLE__ */
---
+++ trunk/ricpp2/source/include/RiCPPTriangle.h
@@ -0,0 +1,8 @@
+#ifndef __RICPPBASE__
+#define __RICPPBASE__
+
+#ifndef _RICPP_BASERENDERER_BASERENDERER_H
+#include <ricpp/baserenderer/baserenderer.h>
+#endif
+
+#endif  /* __RICPPBASE__ */

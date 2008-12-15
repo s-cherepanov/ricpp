ea55790ddecddf4488868b56b6170a8cb7ed3f2c
pidde <pidde@b61f6476-052f-0410-ab0a-e49323056755>
2008-12-15 14:07:01 +0000

    mac compile abrib

--- abloader/source/ABLoaderPlugin/ABLoaderPlugin.cpp
+++ abloader/source/ABLoaderPlugin/ABLoaderPlugin.cpp
@@ -7,16 +7,46 @@
  *
  */
 #include "abloader/ABLoaderPlugin/ABLoaderPlugin.h"
+#include "abloader/abloader/abloader.h"
 
 #pragma GCC visibility push(hidden)
-#include "ricpp/ri/ri.h"
+
+#ifndef _RICPP_RICPP_RICPPBRIDGE_H
+#include "ricpp/ricppbridge/ricppbridge.h"
+#endif // _RICPP_RICPP_RICPPBRIDGE_H
 #include <iostream>
+
 #pragma GCC visibility pop
+
+static RiCPP::CRiCPPBridge *ri = 0; ///< The bridge to the rendering context
+static RiCPP::TemplPluginFactory<RiCPP::CABLoaderCreator> *abLoaderFactory = 0;
 
 using namespace AB;
 
+
+static void SetRoot()
+{
+	if ( ri == 0 ) {
+		ri = new RiCPP::CRiCPPBridge;
+		if ( ri ) {
+			// Register abloader 'renderer' -  can also be omitted and to load libabloader.1.dylib dynamically
+			// for static use link module with libabloader.a for dynamic use link with libricppbridge.a
+			abLoaderFactory = new RiCPP::TemplPluginFactory<RiCPP::CABLoaderCreator>;
+			if ( abLoaderFactory ) {
+				ri->registerRendererFactory("abribloader", reinterpret_cast<RiCPP::TemplPluginFactory<RiCPP::CContextCreator> *>(abLoaderFactory));
+			}
+		}
+	}
+}
+
+static void UnsetRoot()
+{
+	if ( ri )
+		delete ri;
+	ri = 0;
+}
+
 ABLoaderPlugin *ABLoaderPlugin::plugin_instance = 0;
-// RiCPP::TemplPluginFactory<RiCPP::CABLoaderCreator> *ABLoaderPlugin::abLoaderFactory;
 
 ABLoaderPlugin::ABLoaderPlugin()
 {
@@ -29,6 +59,7 @@ ABLoaderPlugin::~ABLoaderPlugin()
 ABLoaderPlugin* ABLoaderPlugin::MyInstance()
 {
 	if ( plugin_instance == 0 ) {
+		SetRoot();
 		plugin_instance = new ABLoaderPlugin;
 	}
 	return plugin_instance;
@@ -41,6 +72,7 @@ void ABLoaderPlugin::deleteInstance(ABLoaderPlugin *plugin)
 	}
 	
 	if ( plugin_instance ) {
+		UnsetRoot();
 		delete plugin_instance;
 		plugin_instance = 0;
 	}
@@ -58,9 +90,11 @@ Loader* ABLoaderPlugin::Instance(ParamSet* paramSet)
 
 void ABLoaderPlugin::load(std::string const &filename)
 {
-	RiBegin("abribloader");
-	RiReadArchive(filename.c_str(), 0, RI_NULL);
-	RiEnd();
+	const char *searchPath = "/usr/local/lib/Augenblick:&";
+	ri->control("searchpath", "renderer", &searchPath, RI_NULL);
+	ri->begin("abribloader");
+	ri->readArchive(filename.c_str(), 0, RI_NULL);
+	ri->end();
 }
 
 extern "C" AB::Plugin* Instance()

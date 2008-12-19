812022e523f754e1b05df58d610dfaf44458580a
pidde <pidde@b61f6476-052f-0410-ab0a-e49323056755>
2008-12-19 13:11:35 +0000

    Changed include tree

--- ricpp2/source/include/RiCPP.h
+++
@@ -1,8 +0,0 @@
-#ifndef __RICPPDEV__
-#define __RICPPDEV__
-
-#ifndef _RICPP_RI_RI_H
-#include <ricpp/ri/ri.h>
-#endif
-
-#endif   /* __RICPPDEV__ */
---
+++ ricpp2/source/include/ricpp/ricpp.h
@@ -0,0 +1,8 @@
+#ifndef __RICPP__
+#define __RICPP__
+
+#ifndef _RICPP_RI_RI_H
+#include <ricpp/ri/ri.h>
+#endif
+
+#endif   /* __RICPP__ */

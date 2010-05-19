8e4594da8fb212c0156a6ebac53962e82adc1343
pidde <pidde@b61f6476-052f-0410-ab0a-e49323056755>
2010-05-19 16:32:48 +0000

    Added linux loader for shared libraries

---
+++ ricpp2/source/gendynlib/linuxdynlib.cpp
@@ -0,0 +1,31 @@
+// RICPP - RenderMan(R) Interface CPP Language Binding
+//
+//     RenderMan(R) is a registered trademark of Pixar
+// The RenderMan(R) Interface Procedures and Protocol are:
+//         Copyright 1988, 1989, 2000, 2005 Pixar
+//                 All rights Reservered
+//
+// Copyright (c) of RiCPP 2007, Andreas Pidde
+// Contact: andreas@pidde.de
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+/** @file linuxdynlib.cpp
+ *  @author Andreas Pidde (andreas@pidde.de)
+ *  @brief Implementation of a class to handle dynamic libraries for Linux (same functionality as MacOS X).
+ */
+
+#include "macdynlib.cpp"
+

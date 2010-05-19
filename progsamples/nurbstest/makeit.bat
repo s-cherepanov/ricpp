462c22fbff5aa49126f9d7fbca81cb4c1aa1e8a5
pidde <pidde@b61f6476-052f-0410-ab0a-e49323056755>
2010-05-19 15:31:53 +0000

    Build Flags

--- progsamples/nurbstest/makeit.bat
+++ progsamples/nurbstest/makeit.bat
@@ -1,8 +1,14 @@
 @echo off
+
 del nurbstest.exe
+
 echo g++ -c src\*.cpp
-g++ -c src\*.cpp
+
+g++ -Wall -fexceptions -g2 -c src\*.cpp
+
 echo g++ -o nurbstest -fexceptions *.o
+
 g++ -o nurbstest.exe -fexceptions *.o
+
 del *.o
 
